e3455e2345236b67dc630c1361742fe520588ae9
limengke <752140235@qq.com>
2019-06-17 17:34:07 +0800

    feat(sg2ts): 修改sg2ts的样式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './style/index.css'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/javascript/javascript'
+import './style/index.scss'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
--- src/page/sg2ts.js
+++ src/page/sg2ts.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { sg2ts } from 'sg2ts'
+import { UnControlled as CodeMirror } from 'react-codemirror2'
+import styles from '../style/page/sg2ts.module.scss'
 
 export default class Sg2ts extends React.Component {
     state = {
         value: ''
     }
 
-    handleChange = e => {
+    handleChange = (editor, data, codeValue) => {
         this.setState({
-            value: e.target.value
+            value: codeValue
         })
     }
 
@@ -18,16 +20,43 @@ export default class Sg2ts extends React.Component {
             space: 4
         })
         return (
-            <div>
-                <textarea
-                    name=""
-                    id=""
-                    cols="30"
-                    rows="10"
-                    value={value}
-                    onChange={this.handleChange}
-                />
-                <div style={{ whiteSpace: 'pre-wrap' }}>{target}</div>
+            <div className={styles.container}>
+                <h1 className={styles.title}>
+                    swagger 数据格式转换成 typescript 格式
+                </h1>
+                <main className={styles.body}>
+                    <div className={styles.code}>
+                        <h2 className="center">输入swagger数据</h2>
+                        <CodeMirror
+                            value={value}
+                            // options={options}
+                            options={{
+                                mode: 'text/javascript',
+                                theme: 'material',
+                                lineNumbers: true,
+                                matchBrackets: true,
+                                lineWrapping: true,
+                                styleActiveLine: true
+                            }}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className={styles.result}>
+                        <h2 className="center">生成的typescript数据</h2>
+                        <CodeMirror
+                            value={target}
+                            options={{
+                                mode: 'text/typescript',
+                                lineNumbers: true,
+                                theme: 'material',
+                                readOnly: true,
+                                lineWrapping: true,
+                                styleActiveLine: true,
+                                matchBrackets: true
+                            }}
+                        />
+                    </div>
+                </main>
             </div>
         )
     }
