d29d01016f5c2fa435a40eae9f0e0743de613259
limengke <752140235@qq.com>
2020-01-20 15:07:25 +0800

    refactor(sg2ts): hooks 替代 class 组件

--- src/page/sg2ts.tsx
+++ src/page/sg2ts.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 import { sg2ts } from 'sg2ts'
 import { Controlled } from 'react-codemirror2'
 import Button from 'antd/lib/button'
@@ -6,28 +6,15 @@ import message from 'antd/lib/message'
 import { copy } from '../util'
 import styles from '../style/page/sg2ts.module.scss'
 
-export interface Sg2tsState {
-    value: string
-}
-
-export default class Sg2ts extends React.Component<{}, Sg2tsState> {
-    state = {
-        value: ''
+export default function Sg2ts() {
+    const [value, setValue] = useState('')
+    const getResult = () => {
+        return sg2ts(value, {
+            space: 4
+        })
     }
-
-    handleChange = (editor: any, data: any, codeValue: string) => {
-        const {
-            state: { value }
-        } = this
-        if (value !== codeValue) {
-            this.setState({
-                value: codeValue
-            })
-        }
-    }
-
-    handleCopy = () => {
-        const result = this.getResult()
+    const handleCopy = () => {
+        const result = getResult()
         const { error } = copy(result)
         if (error) {
             message.error(error)
@@ -35,89 +22,74 @@ export default class Sg2ts extends React.Component<{}, Sg2tsState> {
             message.success('复制成功')
         }
     }
-
-    handleClear = () => {
-        const { value } = this.state
+    const handleClear = () => {
         if (value !== '') {
-            this.setState(
-                {
-                    value: ''
-                },
-                () => {
-                    message.success('清除成功')
-                }
-            )
+            setValue('')
+            message.success('清除成功')
         }
     }
-
-    getResult = () => {
-        const { value } = this.state
-        return sg2ts(value, {
-            space: 4
-        })
+    const handleChange = (editor: any, data: any, codeValue: string) => {
+        if (value !== codeValue) {
+            setValue(codeValue)
+        }
     }
-
-    render() {
-        const { value } = this.state
-        const target = this.getResult()
-        return (
-            <div className={styles.container}>
-                <h1 className={styles.title}>
-                    swagger 数据格式转换成 typescript 格式
-                </h1>
-                <main className={styles.body}>
-                    <div className={styles.code}>
-                        <div className={styles['sub-title-container']}>
-                            <span className={styles['sub-title']}>
-                                输入swagger数据
-                            </span>
-                            <Button
-                                size="small"
-                                type="primary"
-                                onClick={this.handleClear}
-                            >
-                                清除
-                            </Button>
-                        </div>
-                        <Controlled
-                            value={value}
-                            options={{
-                                mode: 'text/javascript',
-                                theme: 'material',
-                                lineNumbers: true,
-                                lineWrapping: true
-                            }}
-                            onBeforeChange={this.handleChange}
-                            // onChange={this.handleChange}
-                        />
+    const target = useMemo(getResult, [value])
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                swagger 数据格式转换成 typescript 格式
+            </h1>
+            <main className={styles.body}>
+                <div className={styles.code}>
+                    <div className={styles['sub-title-container']}>
+                        <span className={styles['sub-title']}>
+                            输入swagger数据
+                        </span>
+                        <Button
+                            size="small"
+                            type="primary"
+                            onClick={handleClear}
+                        >
+                            清除
+                        </Button>
                     </div>
-                    <div className={styles.result}>
-                        <div className={styles['sub-title-container']}>
-                            <span className={styles['sub-title']}>
-                                生成的typescript数据
-                            </span>
-                            <Button
-                                size="small"
-                                type="primary"
-                                onClick={this.handleCopy}
-                            >
-                                复制
-                            </Button>
-                        </div>
-                        <Controlled
-                            onBeforeChange={() => {}}
-                            value={target}
-                            options={{
-                                mode: 'text/typescript',
-                                lineNumbers: true,
-                                theme: 'material',
-                                readOnly: true,
-                                lineWrapping: true
-                            }}
-                        />
+                    <Controlled
+                        value={value}
+                        options={{
+                            mode: 'text/javascript',
+                            theme: 'material',
+                            lineNumbers: true,
+                            lineWrapping: true
+                        }}
+                        onBeforeChange={handleChange}
+                    />
+                </div>
+                <div className={styles.result}>
+                    <div className={styles['sub-title-container']}>
+                        <span className={styles['sub-title']}>
+                            生成的typescript数据
+                        </span>
+                        <Button
+                            size="small"
+                            type="primary"
+                            onClick={handleCopy}
+                        >
+                            复制
+                        </Button>
                     </div>
-                </main>
-            </div>
-        )
-    }
+                    <Controlled
+                        onBeforeChange={() => {}}
+                        value={target}
+                        options={{
+                            mode: 'text/typescript',
+                            lineNumbers: true,
+                            theme: 'material',
+                            readOnly: true,
+                            lineWrapping: true
+                        }}
+                    />
+                </div>
+            </main>
+        </div>
+    )
 }
